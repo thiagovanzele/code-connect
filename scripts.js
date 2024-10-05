bf576669ce28aa1c8470e523a8d811c8f553ee94
Thiago Vanzele <thiagovanzele@gmail.com>
2024-10-05 14:40:32 -0300

    Funcoes para adicionar e remover tags

--- scripts.js
+++ scripts.js
@@ -3,21 +3,21 @@ const inputUpload = document.getElementById("imagem-upload");
 
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
-})
+});
 
 function lerConteudoDoArquivo(arquivo) {
     return new Promise((resolve, reject) => {
         const leitor = new FileReader();
         leitor.onload = () => {
-            resolve({url: leitor.result, nome: arquivo.name})
-        }
+            resolve({ url: leitor.result, nome: arquivo.name });
+        };
 
         leitor.onerror = () => {
-            reject(`Erro na leitura do arquivo ${arquivo.name}`)
-        }
+            reject(`Erro na leitura do arquivo ${arquivo.name}`);
+        };
 
-        leitor.readAsDataURL(arquivo)
-    }) 
+        leitor.readAsDataURL(arquivo);
+    });
 }
 
 const imagemPrincipal = document.querySelector(".main-imagem");
@@ -35,4 +35,55 @@ inputUpload.addEventListener("change", async (evento) => {
             console.error("Erro na leitura do arquivo");
         }
     }
-})
+});
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+listaTags.addEventListener("click", (evento) => {
+    if (evento.target.classList.contains("remove-tag")) {
+        const tagASerRemovida = evento.target.parentElement;
+        listaTags.removeChild(tagASerRemovida);
+    }
+});
+
+const tagsDisponiveis = [
+    "Front-end",
+    "Programação",
+    "Data Science",
+    "Full-stack",
+    "HTML",
+    "CSS",
+    "JavaScript",
+];
+
+async function verificaTagsDisponíveis(tagTexto) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(tagsDisponiveis.includes(tagTexto));
+        }, 1000);
+    });
+}
+
+inputTags.addEventListener("keypress", async (evento) => {
+    if (evento.key === "Enter") {
+        evento.preventDefault();
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== "") {
+            try {
+                const tagExiste = await verificaTagsDisponíveis(tagTexto);
+                if (tagExiste) {
+                    const novaTag = document.createElement("li");
+                    novaTag.innerHTML = `<p>${tagTexto}</p> <img src="/img/close-black.svg" class="remove-tag">`;
+                    listaTags.appendChild(novaTag);
+                    inputTags.value = "";
+                } else {
+                    alert("Tag não foi encontrada");
+                }
+            } catch (error) {
+                console.error("Erro ao verificar a existência da tag");
+                alert("Erro ao verifficar a existência da tag.");
+            }
+        }
+    }
+});
